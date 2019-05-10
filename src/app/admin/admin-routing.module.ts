41abb55d752ffc580bd64d62377f2de1b8c252ab
unknown <al02728814@tecmilenio.mx>
2019-05-10 00:53:15 -0500

    Auth Guard

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -3,12 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { ProductFormComponent } from './product-form/product-form.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { AuthGuardService } from '../services/auth-guard.service';
 
 const routes: Routes = [
   {path: "login", component:LoginComponent},
-  {path: "product/:id", component:ProductFormComponent},
-  {path: "product", component:ProductFormComponent},
-  {path: "products", component:ProductListComponent},
+  {path: "product/:id", component:ProductFormComponent, canActivate:[AuthGuardService]},
+  {path: "product", component:ProductFormComponent, canActivate:[AuthGuardService]},
+  {path: "products", component:ProductListComponent, canActivate:[AuthGuardService]},
 ];
 
 @NgModule({
